69118470cc39180429d25b8f37f728fe8ee4e649
lfrledh <lfrledh@gmail.com>
2022-03-28 17:54:55 +0800

    把arduino下的零零碎碎移过来，避免出现整个目录被推送的问题。

---
+++ build/CMakeFiles/kaishila.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kaishila.dir/kaishila.cpp.o"
+  "libkaishila.a"
+  "libkaishila.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kaishila.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ririnnkai/文档/code git
+# Build directory: /home/ririnnkai/文档/code git/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
